b857724510a17f17028e2fd0353d153f52a3d6cc
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-04-15 03:10:41 -0700

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,7 +1,6 @@
 class Example {
 
-  public static void main(String[] args) 
-             {
+  public static void main(String[] args){
     
     System.out.println("Enter two numbers");
              int first = 10;
